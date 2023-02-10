6ee208581612f63483554a1449b18822166275cf
leonardocandio <leonardo.candio@utec.edu.pe>
2023-02-10 18:26:41 -0500

    ➕Conan package manager is now required

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include "app/blockchain.h"
 #include "iostream"
-
+#include "crow/crow_all.h"
 
 int main(int argc, char *argv[]) {
-    blockchain *safe = new blockchain();
-
-    safe->addBlock("Hola");
-    safe->addBlock("Hola1");
-    safe->addBlock("Hola2");
-    safe->addBlock("Hola3");
-    safe->addFromFile("/Users/leonardocandio/Desktop/UTEC/2023-0/EDA/proyecto-blockchain/array/transactions.csv");
-
-    std::cout << safe->isChainValid();
 }
 

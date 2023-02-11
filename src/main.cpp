3fc544def9597528e957befa84d91cd6be34b2ac
leonardocandio <leonardo.candio@utec.edu.pe>
2023-02-10 19:11:10 -0500

    ✨ Now using an API

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include "app/blockchain.h"
-#include "iostream"
 #include "crow/crow_all.h"
 
+
+//handle get request
+crow::json::wvalue handleGET(const crow::request &req, crow::response &res) {
+    crow::json::wvalue x;
+    x["message"] = "Hello World!";
+    return x;
+}
+
+crow::json::wvalue handlePOST(const crow::request &req, crow::response &res) {
+    crow::json::wvalue x;
+    x["message"] = "Hello World!";
+    return x;
+}
+
 int main(int argc, char *argv[]) {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")
+            .methods("GET"_method, "POST"_method)
+                    ([](const crow::request &req) {
+                        crow::response response;
+                        switch (req.method) {
+                            case crow::HTTPMethod::GET:
+                                response.json_value = handleGET(req, response);
+                                return response;
+                            case crow::HTTPMethod::POST:
+                                response.json_value = handlePOST(req, response);
+                                return response;
+                            default:
+                                return crow::response(404);
+                        }
+                    });
+
+    app.port(18080).multithreaded().run();
 }
 

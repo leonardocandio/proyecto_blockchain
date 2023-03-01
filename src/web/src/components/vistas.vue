364438adfd877918b61cdacd96c4665a22caf47a
leonardocandio <leonardo.candio@utec.edu.pe>
2023-03-01 16:35:09 -0500

    styling change

--- src/web/src/components/vistas.vue
+++ src/web/src/components/vistas.vue
@@ -1,50 +1,66 @@
 <template>
   <div class="container section">
     <h4>
-      <center>Vistas de los bloques dentro de la API</center>
+      Vistas de los bloques dentro de la API
     </h4>
-    <div class="row">
-      <center>
-        <div class="col s4">
-          <center>
-            <div v-for="block in blocks" :key="blocks" class="card sticky-action large">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator"
-                     src="https://www.rentafija.com/wp-content/uploads/2022/02/como-funciona-el-blockchain-1.jpg">
-              </div>
-              <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4">HASH: {{ block.hash }}<i
-                    class="material-icons right">more_vert</i></span>
-              </div>
-              <div class="card-action">
-                <p><a href="/transactions" v-on:click="Click(block.index)">Transactions</a></p>
-              </div>
-              <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4">HASH: {{ block.hash }}<i class="material-icons right">close</i></span>
-                <p>Nonce: {{ block.nonce }}</p>
-                <p>Index: {{ block.index }}</p>
-                <p>Previus hashMap: {{ block.prevhash }}</p>
-                <p>timestamp: {{ block.timestamp }}</p>
-              </div>
-            </div>
-          </center>
+    <!--horizontal scrolling cards    -->
+    <div class="row-b">
+      <div v-for="block in blocks" :key="blocks" class="card-b card sticky-action large">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator"
+               src="https://www.rentafija.com/wp-content/uploads/2022/02/como-funciona-el-blockchain-1.jpg">
         </div>
-      </center>
+        <div class="card-content">
+                <span class="activator grey-text text-darken-4">Hash: {{ block.hash }}<i
+                  class="material-icons right">more_vert</i></span>
+        </div>
+        <div class="card-action">
+          <p><a href="/transactions" v-on:click="Click(block.index)">Transactions</a></p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Block<i
+            class="material-icons right">close</i></span>
+          <p>hash: {{ block.hash }}</p>
+          <p>Nonce: {{ block.nonce }}</p>
+          <p>Index: {{ block.index }}</p>
+          <p>PrevHash: {{ block.prevhash }}</p>
+          <p>Timestamp: {{ block.timestamp }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+/*
+infinite horizontal scrolling cards
+*/
+.card-b {
+  display: inline-block;
+  margin: 1rem;
+  width: 30rem;
+  height: 20rem;
+  position: relative;
+  overflow: hidden;
+}
+
+.row-b {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+</style>
 
 <script>
 import Services from "@/services/Services";
 import M from "materialize-css";
 
-const charge = localStorage.getItem('loading');
-var charge_num = parseInt(charge);
+const charge = localStorage.getItem("loading");
+let charge_num = parseInt(charge);
 
 export default {
   data() {
     return {
-      blocks: [],
+      blocks: []
     };
   },
 
@@ -62,25 +78,25 @@ export default {
     getBlocks() {
       Services.getBlocks().then((response) => {
         this.blocks = response.data.blocks;
-      })
+      });
     },
 
     Click(index) {
-      if (localStorage.getItem('Index') != null) {
-        localStorage.removeItem('Index')
+      if (localStorage.getItem("Index") != null) {
+        localStorage.removeItem("Index");
       }
-      localStorage.setItem('Index', index)
+      localStorage.setItem("Index", index);
     },
 
     TransacctionsView() {
       if (charge_num != 0) {
-        localStorage.removeItem('loading')
-        window.location.reload()
+        localStorage.removeItem("loading");
+        window.location.reload();
       }
-      localStorage.setItem('loading', 0)
-    },
+      localStorage.setItem("loading", 0);
+    }
   }
-}
+};
 
 
 </script>
